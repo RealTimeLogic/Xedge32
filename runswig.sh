f89cf196ce825cc42cd8e1daba62ca0dbbefa30d
RTL-pub <ginfo@realtimelogic.com>
2022-01-28 12:40:51 -0800

    Update runswig.sh

--- runswig.sh
+++ runswig.sh
@@ -1,3 +1,14 @@
-CPWD=$PWD
-cd ..
-swig -DCONFIG_IDF_TARGET_ESP32=1 -Iesp-idf/components/esp32/ -Iesp-idf/components/esp_common/include -Iesp-idf/components/hal/include -Iesp-idf/components/soc/include -Iesp-idf/components/soc/esp32/include -Iesp-idf/components/driver/include -Iesp-idf/components/esp_rom/include -Iesp-idf/components/driver/esp32/include -Iesp-idf/components/nvs_flash/include -I$CPWD/main -lua -o $CPWD/main/esp_wrap.c $CPWD/tools/esp.i
+function installswig {
+    read -p "SWIG is not installed. Do you want to install SWIG (Y/n)?"
+    if [ "$REPLY" == "n" ]; then
+        return
+    fi
+    sudo apt -y install swig
+}
+command -v swig >/dev/null 2>&1 || installswig
+
+pushd ~/esp/esp-idf || abort
+IDFP=$PWD
+popd
+
+swig -DCONFIG_IDF_TARGET_ESP32=1 -I$IDFP/components/esp32/ -I$IDFP/components/esp_common/include -I$IDFP/components/hal/include -I$IDFP/components/soc/include -I$IDFP/components/soc/esp32/include -I$IDFP/components/driver/include -I$IDFP/components/esp_rom/include -I$IDFP/components/driver/esp32/include -I$IDFP/components/nvs_flash/include -Imain -lua -o main/esp_wrap.c tools/esp.i
