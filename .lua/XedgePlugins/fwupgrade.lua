682886384a095e21140b3c6b98a283236e6a202e
RTL <RTL-pub@users.noreply.github.com>
2023-10-25 14:54:43 -0700

    Adding OTA upgrade

---
+++ .lua/XedgePlugins/fwupgrade.lua
@@ -0,0 +1,35 @@
+local commands=...
+local busy=false
+
+local function senderror(cmd,msg)
+   cmd:setheader("X-Error",msg)
+   cmd:senderror(503, msg)
+   cmd:abort()
+end
+
+function commands.getfwver(cmd)
+   local v=esp32.ota()
+   v.sha256=nil
+   cmd:json(v)
+end
+
+function commands.uploadfw(cmd)
+   cmd:allow{"PUT"}
+   if busy then senderror(cmd,"Busy: processing firmware upgrade") end
+   local ok
+   local ota,err=esp32.ota"begin"
+   if ota then
+      for data in cmd:rawrdr(4096) do
+	 ok,err=ota:write(data)
+	 if not ok then break end
+      end
+      if ok then ok,err=ota:commit() end
+   end
+   busy=false
+   if not ok then
+      senderror(cmd,"firmware upgrade failed: "..tostring(err))
+   end
+   ba.timer(function() esp32.execute"restart" end):set(500,true)
+   cmd:setstatus(204)
+   cmd:abort()
+end
